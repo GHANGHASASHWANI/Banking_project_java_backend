37731b524a927ac952df5728ff05e2271b723b5c
Ashwani <127748575+GHANGHASASHWANI@users.noreply.github.com>
2025-02-14 09:19:16 +0530

    Update TransactionManagement.java

--- Banking_management_system/bank/src/main/java/com/banking/Service/TransactionManagement.java
+++ Banking_management_system/bank/src/main/java/com/banking/Service/TransactionManagement.java
@@ -1,6 +1,25 @@
 package com.banking.Service;
 import com.banking.Model.*;
+import com.banking.Exception.*;
 
 public class TransactionManagement {
-    
+    public static void main(String[] args) {
+        Account account=new Account();
+        account.setBalance(1000);
+        double amount=2000;
+        try{
+            checkBalance(account,amount);
+        }
+        catch(InsufficientFund e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void checkBalance(Account account,double amount){
+        if(account.getBalance()<amount){
+            throw new InsufficientFund("Insufficient balance in your account to withdraw ");
+        }
+        else{
+            System.out.println("Amount withdrawn successfully");
+        }
+    }
 }
